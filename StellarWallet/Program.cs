1f6b285496fb212040deacb579b2b9d753a81945
Meng Guang <xmgu2008@163.com>
2018-03-13 19:32:28 +0800

    add CreateAccount method.
    Renew the development server.

--- StellarWallet/Program.cs
+++ StellarWallet/Program.cs
@@ -6,8 +6,8 @@ namespace StellarWallet
 {
     class Program
     {
-        public static string seed = "SBKDCHYZHNANMNPFUKFKDIAHEL2K3GLNKKRARQWSE67TTEHTGNGGRKKH";
-        //public static string address = "GCKVX5H5XVVDE653PU7SWDDZEAMBHRY6ECTDVLRCFFHNP33R2JOLRPXC";
+        private const string NetworkPassphrase = "Test Newton Network ; 2018-02-27";
+        public static string seed = "SDGGEW2VCBNYP5FQE2EKZU3NWZAKUF4GJA4SQ47LV7PA2VKAEYXZA7KJ";
         public static string horizonUrl = "http://47.91.208.241:8000";
         public static async Task Main(string[] args)
         {
@@ -17,7 +17,8 @@ namespace StellarWallet
 
             await GetAccountBalance(keyPair);
             //await CreateRandomAccount();
-            var destID = "GB2SX6YMOSLLNFJRHTV4C3QCRBMPL34UY2PRQH3YA7ZMU2ZGIPK5NVW2";
+            var destID = "GCKVX5H5XVVDE653PU7SWDDZEAMBHRY6ECTDVLRCFFHNP33R2JOLRPXC";
+            //await CreateAccount(KeyPair.FromAccountId(destID));
             await MakePayment(destID, 99);
             await GetAccountBalance(keyPair);
             await GetAccountBalance(KeyPair.FromAccountId(destID));
@@ -37,7 +38,7 @@ namespace StellarWallet
 
         public async static Task MakePayment(string destAccountID,int amount)
         {
-            Network.Use(new Network("Test Newton Network ; 2018-02-27"));
+            Network.Use(new Network(NetworkPassphrase));
             var destAccountKeyPair = KeyPair.FromAccountId(destAccountID);
 
             var server = new Server(horizonUrl);
@@ -68,9 +69,42 @@ namespace StellarWallet
             }
         }
 
+        public async static Task CreateAccount(KeyPair destAccount)
+        {
+            Network.Use(new Network(NetworkPassphrase));
+            Console.WriteLine(destAccount.Address);
+            //Console.WriteLine(destAccount.SecretSeed);
+            var server = new Server(horizonUrl);
+            var sourceKeypair = KeyPair.FromSecretSeed(seed);
+            var sourceAccountResp = await server.Accounts.Account(sourceKeypair);
+            var sourceAccount = new Account(sourceKeypair, sourceAccountResp.SequenceNumber);
+            var operation = new CreateAccountOperation.Builder(destAccount, "100").SetSourceAccount(sourceKeypair).Build();
+            var transaction = new Transaction.Builder(sourceAccount).AddOperation(operation).AddMemo(Memo.Text("Hello Memo")).Build();
+            transaction.Sign(sourceKeypair);
+
+            try
+            {
+                var resp = await server.SubmitTransaction(transaction);
+                if (resp.IsSuccess())
+                {
+                    Console.WriteLine("transaction completed successfully!");
+                    await GetAccountBalance(destAccount);
+                }
+                else
+                {
+                    Console.WriteLine("transaction failed.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
         public async static Task CreateRandomAccount()
         {
-            Network.Use(new Network("Test Newton Network ; 2018-02-27"));
+            Network.Use(new Network(NetworkPassphrase));
             var destAccount = KeyPair.Random();
             Console.WriteLine(destAccount.Address);
             Console.WriteLine(destAccount.SecretSeed);
