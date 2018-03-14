015370113e4665c7e4324b02fa5d43aa7b7cb5be
Meng Guang <xmgu2008@163.com>
2018-03-14 23:38:23 +0800

    add config file and command arguments

--- StellarWallet/Program.cs
+++ StellarWallet/Program.cs
@@ -1,28 +1,94 @@
 ﻿using System;
 using stellar_dotnetcore_sdk;
 using System.Threading.Tasks;
+using Nett;
+using DocoptNet;
 
 namespace StellarWallet
 {
+    class WalletConfig
+    {
+        public string NetworkPassphrase { get; set; }
+        public string HorizonUrl { get; set; }
+        public string AccountSeed { get; set; }
+    }
     class Program
     {
-        private const string NetworkPassphrase = "Test Newton Network ; 2018-02-27";
-        public static string seed = "SDGGEW2VCBNYP5FQE2EKZU3NWZAKUF4GJA4SQ47LV7PA2VKAEYXZA7KJ";
-        public static string horizonUrl = "http://47.91.208.241:8000";
+        private const string usage = @"
+
+    Usage:
+      stellarwallet new_account
+      stellarwallet active_account <account_id>
+      stellarwallet check_balance <account_id>
+      stellarwallet pay <account_id> <amount>
+      stellarwallet (-h | --help)
+      stellarwallet --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+
+    ";
+        private static string NetworkPassphrase = "";
+        private static string seed = "";
+        private static string horizonUrl = "";
+
+        public static bool ReadWalletConfig()
+        {
+            try
+            {
+                var config = Toml.ReadFile<WalletConfig>("config.tml");
+                NetworkPassphrase = config.NetworkPassphrase;
+                seed = config.AccountSeed;
+                horizonUrl = config.HorizonUrl;
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+        }
+
         public static async Task Main(string[] args)
         {
-            var keyPair = KeyPair.FromSecretSeed(seed);
-            var address = keyPair.Address;
-            Console.WriteLine(address);
-
-            await GetAccountBalance(keyPair);
-            //await CreateRandomAccount();
-            var destID = "GCKVX5H5XVVDE653PU7SWDDZEAMBHRY6ECTDVLRCFFHNP33R2JOLRPXC";
-            //await CreateAccount(KeyPair.FromAccountId(destID));
-            await MakePayment(destID, 99);
-            await GetAccountBalance(keyPair);
-            await GetAccountBalance(KeyPair.FromAccountId(destID));
-
+            if (ReadWalletConfig() == false)
+            {
+                return;
+            }
+            var arguments = new Docopt().Apply(usage, args, version: "StellarWallet 1.0", exit: true);
+            foreach (var argument in arguments)
+            {
+                Console.WriteLine("{0} = {1}", argument.Key, argument.Value);
+            }
+            if(arguments["new_account"].IsTrue)
+            {
+                var destAccount = KeyPair.Random();
+                Console.WriteLine(destAccount.Address);
+                Console.WriteLine(destAccount.SecretSeed);
+                return;
+            }
+            if (arguments["check_balance"].IsTrue)
+            {
+                var destAccount = KeyPair.FromAccountId(arguments["<account_id>"].ToString());
+                await GetAccountBalance(destAccount);
+                return;
+            }
+            if (arguments["active_account"].IsTrue)
+            {
+                var destAccount = KeyPair.FromAccountId(arguments["<account_id>"].ToString());
+                await CreateAccount(destAccount);
+                return;
+            }
+            if (arguments["pay"].IsTrue)
+            {
+                var destAccount = KeyPair.FromAccountId(arguments["<account_id>"].ToString());
+                var amount = arguments["<amount>"].AsInt;
+                await MakePayment(destAccount.AccountId,amount);
+                return;
+            }
+            return;
         }
 
         public async static Task GetAccountBalance(KeyPair keyPair)
@@ -78,7 +144,7 @@ namespace StellarWallet
             var sourceKeypair = KeyPair.FromSecretSeed(seed);
             var sourceAccountResp = await server.Accounts.Account(sourceKeypair);
             var sourceAccount = new Account(sourceKeypair, sourceAccountResp.SequenceNumber);
-            var operation = new CreateAccountOperation.Builder(destAccount, "100").SetSourceAccount(sourceKeypair).Build();
+            var operation = new CreateAccountOperation.Builder(destAccount, "20").SetSourceAccount(sourceKeypair).Build();
             var transaction = new Transaction.Builder(sourceAccount).AddOperation(operation).AddMemo(Memo.Text("Hello Memo")).Build();
             transaction.Sign(sourceKeypair);
 
@@ -93,6 +159,12 @@ namespace StellarWallet
                 else
                 {
                     Console.WriteLine("transaction failed.");
+                    var c = resp.SubmitTransactionResponseExtras.ExtrasResultCodes;
+                    Console.WriteLine(c.TransactionResultCode);
+                    foreach(var x in c.OperationsResultCodes)
+                    {
+                        Console.WriteLine(x);
+                    }
                 }
             }
             catch (Exception e)
